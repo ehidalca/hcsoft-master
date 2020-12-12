4844e660864aff70dc92e4247db6ec2a8e468172
Eduardo <ehidalgoca@gmail.com>
2020-12-11 21:12:40 -0300

    prueba

--- windowsPOS/WindowsPOS/FrmLogin.cs
+++ windowsPOS/WindowsPOS/FrmLogin.cs
@@ -19,7 +19,7 @@ namespace HCSoft.POS.WinApp
         private void Form1_Load(object sender, EventArgs e)
         {
 
-
+            string x = "Saludos";
            // MessageBox.Show(Seguridad.Desencriptar(new Usuario().ObtieneUsuario(10).Password.ToString()));
 
             lblError.Text = "";  
